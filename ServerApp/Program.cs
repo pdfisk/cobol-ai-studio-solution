fac6a127be711bab222dc6acc5a3111068bc65be
Peter Fisk <peter.fisk@gmail.com>
2026-03-05 14:40:49 -0500

    server

--- ServerApp/Program.cs
+++ ServerApp/Program.cs
@@ -1,5 +1,17 @@
-﻿using BlazorLib.Interop;
+using BlazorLib.Interop;
 using BlazorLib.Server;
 
-SimpleHttpServer simpleHttpServer = new SimpleHttpServer("http://localhost:8080/", new InteropApi());
-simpleHttpServer.Start();
+var server = new SimpleHttpServer("http://localhost:8080/", new InteropApi());
+server.Start();
+
+using (server)
+{
+    Console.WriteLine("Server running at http://localhost:8080/. Press Ctrl+C to stop.");
+    var exitEvent = new ManualResetEventSlim(false);
+    Console.CancelKeyPress += (_, e) =>
+    {
+        e.Cancel = true;
+        exitEvent.Set();
+    };
+    exitEvent.Wait();
+}
